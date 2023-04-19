25952b2b337d0c46920f134f904a4bd1304fb787
aj2000 <hi@ajaysharma.dev>
2023-04-19 22:07:52 +0530

    read me update

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+const parseCode = (str) => {
+  // your code here
+};
+
+// Do not change the code below
+const str = prompt("Enter str: ");
+alert(JSON.stringify(parseCode(str)));
